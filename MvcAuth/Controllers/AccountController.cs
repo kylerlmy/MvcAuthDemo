87658828f018d2ba37d3dc612f01239ea274afd5
kyle <kylerlimy@gmail.com>
2018-06-02 18:45:58 +0800

    mvc auth

---
+++ MvcAuth/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;     //授权
+using Microsoft.AspNetCore.Authentication;    //证明
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace MvcAuth.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        //[Authorize]  如果启用，将会一直循环跳转
+        public IActionResult MakeLogin()
+        {
+
+            //模拟用户认证登录
+            var claims=new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,"Kyle"),
+                new Claim(ClaimTypes.Role,"Admin"),
+            };
+                var claimIdentity=new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+
+
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimIdentity));
+            return Ok();       //返回一个OK(),它会变成一个Api
+        }
+
+
+        public IActionResult LogOut()
+        {
+            HttpContext.SignOutAsync();
+            return Ok();
+        }
+    }
+
+
+   
+}
---
+++ MvcAuth/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MvcAuth.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();   //这里的View直接定位到Views下的Admin目录下的Index.cshtml文件。（如果Admin文件夹不存在，或者Admin目录的文件的文件名不是Index.cshtml,View()方法都不能成功定位）
+        }
+    }
+}
